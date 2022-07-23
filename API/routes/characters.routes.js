ab8630b8461082e525f5f527474c43211af4e5d9
Aryel Penido <aryel.cruz@ufv.br>
2022-07-23 19:58:20 -0300

    ADD: projects and consuming marvel api

---
+++ API/routes/characters.routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { createUrl, fetchMarvelAPI } = require('../utils/marvelAPI');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const urlString = 'https://gateway.marvel.com/v1/public/characters';
+    const url = createUrl(urlString);
+  
+    const params = Object.keys(req.query);
+    params.forEach((param) => {
+      if (req.query[param] !== '') {
+        url.searchParams.set(param, req.query[param]);
+      }
+    });
+  
+    try {
+      
+      const characters = await fetchMarvelAPI(url); 
+      res.send(characters);
+    } catch (e) {
+      res.status(500).send();
+    }
+});
+
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  console.log(req.params);
+  if (!Number.isNaN(id)) {
+    const urlString = `https://gateway.marvel.com:443/v1/public/characters/${id}`;
+    const url = createUrl(urlString);
+    
+
+    try {
+      const characters = await fetchMarvelAPI(url);
+      res.send(characters);
+    } catch (e) {
+      res.status(500).send();
+    }
+  } else {
+    res.status(500).send({ error: 'Please provide an id!' });
+  }
+});
+
+  
+  module.exports = router;
---
+++ API/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const charactersRouter = require('./routes/characters.routes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/characters/', charactersRouter);
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log('Your routes will be running on http://localhost:3000');
+  });
+
+
+module.exports = app;
